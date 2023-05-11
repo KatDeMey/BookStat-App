6ac6468244a312f641864f482a16d330d519cce5
unknown <katdemey@outlook.com>
2023-05-11 23:10:16 +0100

    Create Book functionality implemented backend

--- backend-bookstat/src/controllers/book.js
+++ backend-bookstat/src/controllers/book.js
@@ -8,25 +8,88 @@ import {
 } from "../domain/book.js";
 
 export const create = async (req, res) => {
-  const { content } = req.body;
-  if (!content) {
-    return sendDataResponse(res, 400, { error: "Must provide content" });
+  const book = {};
+
+  if (req.body.title) {
+    book.title = req.body.title;
+  } else {
+    return sendDataResponse(res, 400, "Please provide a title");
   }
 
-  const createdBook = await createBook(content);
-  return sendDataResponse(res, 201, {
-    data: {
-      id: createdBook.id,
-      title: createdBook.title,
-      authorFirstName: createdBook.authorFirstName,
-      authorLastName: createdBook.authorLastName,
-      coverUrl: createdBook.coverUrl,
-      numPages: createdBook.numPages,
-      publisher: createdBook.publisher,
-      yearPublished: createdBook.yearPublished,
-      ReadStatus: createdBook.ReadStatus,
-    },
-  });
+  if (req.body.authorFirstName) {
+    book.authorFirstName = req.body.authorFirstName;
+  } else {
+    return sendDataResponse(res, 400, "Please provide a authorFirstName");
+  }
+  if (req.body.authorLastName) {
+    book.authorLastName = req.body.authorLastName;
+  } else {
+    return sendDataResponse(res, 400, "Please provide a authorLastName");
+  }
+  if (req.body.coverUrl) {
+    book.coverUrl = req.body.coverUrl;
+  } else {
+    //TODO: check this can be used/ find another img
+    book.coverUrl =
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg03.deviantart.net%2F323e%2Fi%2F2009%2F028%2F8%2F0%2Fclassic_red_book_cover_by_semireal_stock.jpg&f=1&nofb=1&ipt=c870a101338261a09c317afc76f9857273ebf466bb581e76ee56ab03420e13f1&ipo=images";
+  }
+  if (req.body.numPages) {
+    book.numPages = req.body.numPages;
+  } else {
+    book.numPages = null;
+  }
+  if (req.body.publisher) {
+    book.publisher = req.body.publisher;
+  } else {
+    return sendDataResponse(res, 400, "Please provide a publisher");
+  }
+  if (req.body.yearPublished) {
+    book.yearPublished = req.body.yearPublished;
+  } else {
+    book.yearPublished = null;
+  }
+  if (req.body.ReadStatus) {
+    book.ReadStatus = req.body.ReadStatus;
+  } else {
+    book.ReadStatus = "Not Read";
+  }
+
+  try {
+    console.log(book);
+    const newBook = await createBook(
+      book.title,
+      book.authorFirstName,
+      book.authorLastName,
+      book.coverUrl,
+      book.numPages,
+      book.publisher,
+      book.yearPublished,
+      book.ReadStatus
+    );
+    return sendDataResponse(res, 201, newBook);
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error(e.code, e.message);
+      return sendDataResponse(res, 400, "error");
+    } else {
+      console.log(e);
+      return sendDataResponse(res, 400, "error");
+    }
+  }
+
+  // return sendDataResponse(res, 201, {
+  //   data: {
+  //     id: createdBook.id,
+  //     title: createdBook.title,
+  //     authorFirstName: createdBook.authorFirstName,
+  //     authorLastName: createdBook.authorLastName,
+  //     coverUrl: createdBook.coverUrl,
+  //     numPages: createdBook.numPages,
+  //     publisher: createdBook.publisher,
+  //     yearPublished: createdBook.yearPublished,
+  //     ReadStatus: createdBook.ReadStatus,
+  //   },
+  // });
 };
 
 export const getAll = async (req, res) => {
--- backend-bookstat/src/domain/book.js
+++ backend-bookstat/src/domain/book.js
@@ -1,4 +1,4 @@
-import dbClient from '../utils/dbClient.js'
+import dbClient from "../utils/dbClient.js";
 
 export async function createBook(
   title,
@@ -25,25 +25,26 @@ export async function createBook(
 }
 
 export async function getAllBooks() {
-  return await dbClient.book.findMany(
+  return await dbClient.book
+    .findMany
     // {take: 6}
-  );
+    ();
 }
 
 export async function findById(id) {
   return await dbClient.book.findUnique({
     where: {
-      id: id
+      id: id,
     },
-  })
+  });
 }
 
 export async function deleteById(id) {
   return await dbClient.book.delete({
     where: {
-      id
-    }
-  })
+      id,
+    },
+  });
 }
 
 // export async function updateBookById(id, content) {
