d00a48fdd695d748164a372d1f82754f2ed79b67
unknown <katdemey@outlook.com>
2023-10-28 12:50:45 +0100

    installed tailwind css for redesign

--- src/components/pages/Stats.jsx
+++ src/components/pages/Stats.jsx
@@ -7,9 +7,7 @@ import { Doughnut } from "react-chartjs-2";
 import { Bar } from "react-chartjs-2";
 
 const Stats = (allBooks, setAllBooks) => {
-
   let bookList = allBooks.allBooks;
-  console.log("in Stats", bookList);
 
   const readBooks = bookList.filter((book) => book.ReadStatus === "read");
   const reading = bookList.filter((book) => book.ReadStatus === "reading");
@@ -17,7 +15,6 @@ const Stats = (allBooks, setAllBooks) => {
   const notRead = bookList.filter((book) => book.ReadStatus === "notRead");
 
   const smallBooks = bookList.filter((book) => book.numPages < 200);
-  console.log("smallBooks", smallBooks);
   const mediumBooks = bookList.filter((book) => 200 < book.numPages < 499);
   const largeBooks = bookList.filter((book) => book.numPages >= 500);
   const notListed = bookList.filter((book) => !book.numPages);
@@ -46,7 +43,12 @@ const Stats = (allBooks, setAllBooks) => {
     datasets: [
       {
         label: "Number of pages Dataset",
-        data: [smallBooks.length, mediumBooks.length, largeBooks.length, notListed.length],
+        data: [
+          smallBooks.length,
+          mediumBooks.length,
+          largeBooks.length,
+          notListed.length,
+        ],
         backgroundColor: [
           "rgb(255, 99, 132)",
           "rgb(54, 162, 235)",
@@ -58,43 +60,42 @@ const Stats = (allBooks, setAllBooks) => {
     ],
   };
 
-// TODO: BAR CHART
+  // TODO: BAR CHART
+  //labels = author name and lastname
+  const labels = [
+    "brandon Sanderson",
+    "JK Rowling",
+    "William Shakespear",
+    "Umi Sakuri",
+    "Charles Dickens",
+  ];
 
-//labels = author name and lastname
-const labels = ["brandon Sanderson", "JK Rowling", "William Shakespear", "Umi Sakuri", "Charles Dickens"]
-
-const topAuthorsData = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Your Top Authors",
-      //will need to do a filter of authors then .length (probably).
-      //TODO: unhardcode
-      data: [5, 9, 2, 8, 4],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(255, 205, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(201, 203, 207, 0.2)",
-      ],
-      borderColor: [
-        "rgb(255, 99, 132)",
-        "rgb(255, 159, 64)",
-        "rgb(255, 205, 86)",
-        "rgb(75, 192, 192)",
-        "rgb(54, 162, 235)",
-        "rgb(153, 102, 255)",
-        "rgb(201, 203, 207)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-
+  const topAuthorsData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Your Top Authors",
+        //will need to do a filter of authors then .length (probably).
+        //TODO: unhardcode
+        data: [5, 9, 2, 8, 4],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <>
@@ -102,42 +103,33 @@ const topAuthorsData = {
         <Header />
         <Navigation allBooks={allBooks} setAllBooks={setAllBooks} />
         <main main className="mainsection grid">
-          <h1>Reading Stats</h1>
+          <div>
+            <h1>Reading Stats</h1>
 
-          {/* <ul>
+            {/* <ul>
             <li>Total Books: {bookList.length}</li>
             <li>readBooks: {readBooks.length}</li>
             <li>tbr: {tbr.length}</li>
             <li>reading: {reading.length}</li>
             <li>notRead: {notRead.length}</li>
           </ul> */}
+            <div className="flex">
+              <div style={{ height: "300px", width: "300px" }}>
+                <h2>Total Reads Tracker</h2>
+                <Doughnut data={ReadStatusData} />
+              </div>
 
-          <br />
-          <br />
+              <div style={{ height: "300px", width: "300px" }}>
+                <h2>Num Pages</h2>
+                <Doughnut data={NumPagesData} />
+              </div>
+            </div>
+       
 
-          <div style={{ height: "300px", width: "300px" }}>
-            <h2>Total Reads Tracker</h2>
-            <Doughnut data={ReadStatusData} />
-          </div>
-
-          <br />
-          <br />
-          <br />
-          <br />
-
-          <div style={{ height: "300px", width: "300px" }}>
-            <h2>Num Pages</h2>
-            <Doughnut data={NumPagesData} />
-          </div>
-
-          <br />
-          <br />
-          <br />
-          <br />
-
-          <div style={{ height: "300px", width: "300px" }}>
-            <h2>Authors</h2>
-            <Bar data={topAuthorsData} />
+            <div style={{ height: "300px", width: "300px" }}>
+              <h2>Authors</h2>
+              <Bar data={topAuthorsData} />
+            </div>
           </div>
         </main>
 
